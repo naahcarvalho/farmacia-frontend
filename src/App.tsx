dd9f7cab004243b73ce1cdd11b7e22bd45d87e5b
Naah Carvalho <naah_.carvalho@outlook.com>
2025-08-06 14:00:17 -0300

    style: ajustes visuais no App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import './App.css'
-import Footer from './components/footer/Footer'
-import Navbar from './components/navbar/navbar'
-import Home from './pages/home/Home' 
-import CadastrarCategoria from './pages/categoria/CadastrarCategoria' 
-import ListarCategoria from './pages/categoria/ListarCategoria'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Footer from "./components/footer/Footer";
+import Navbar from "./components/navbar/navbar";
+import CadastrarCategoria from "./pages/categoria/CadastrarCategoria";
+import DeletarCategoria from "./pages/categoria/DeletarCategoria";
+import ListarCategoria from "./pages/categoria/ListarCategoria";
+import Home from "./pages/home/Home";
 
 function App() {
   return (
@@ -13,15 +14,17 @@ function App() {
 
       <main className="min-h-[80vh]">
         <Routes>
+          <Route path="/" element={<Home />} />
           <Route path="/home" element={<Home />} />
-          <Route path="/listarCategoria" element={<ListarCategoria />} /> 
+          <Route path="/listarCategoria" element={<ListarCategoria />} />
           <Route path="/cadastrarCategoria" element={<CadastrarCategoria />} />
+          <Route path="/deletarCategoria/:id" element={<DeletarCategoria />} />
         </Routes>
       </main>
 
       <Footer />
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
