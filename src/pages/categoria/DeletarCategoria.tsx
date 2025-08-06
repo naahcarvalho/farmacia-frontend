22f2a656a8a12505cf7755a9fc99b17ea9189fed
Naah Carvalho <naah_.carvalho@outlook.com>
2025-08-06 14:10:13 -0300

    fix: corrigida importação da interface Categoria

--- src/pages/categoria/DeletarCategoria.tsx
+++ src/pages/categoria/DeletarCategoria.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
-import type Categoria from "../../models/Categoria";
+import type { Categoria } from "../../models/Categoria";
 import { buscar, deletar } from "../../services/Service";
 
 function DeletarCategoria() {
